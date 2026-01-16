ee1efe06527793701d52854d1c9b151079d7dbc6
Xolani Shongwe <lanshongwe10@gmail.com>
2026-01-16 18:31:15 +0200

    test application

---
+++ src/main/java/com/example/LoginServlet.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if ("admin".equals(username) && "admin123".equals(password)) {
+            response.sendRedirect("welcome.jsp");
+        } else {
+            response.sendRedirect("index.jsp?error=true");
+        }
+    }
+}
